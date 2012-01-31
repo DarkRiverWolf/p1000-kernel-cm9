9a2f19fb5ac503ffe5edc066279a4637ef72bf58
DarkRiverWolf <derrian.wolf@gmail.com>
2012-01-31 14:51:24 +0000

    nothing to see here

--- build.sh
+++ build.sh
@@ -10,7 +10,11 @@ setup ()
 
     KERNEL_DIR="$(dirname "$(readlink -f "$0")")"
     BUILD_DIR="$KERNEL_DIR/build"
+<<<<<<< HEAD
     MODULES=("fs/cifs/cifs.ko" "fs/fuse/fuse.ko" "fs/nls/nls_utf8.ko")
+=======
+    #MODULES=("fs/cifs/cifs.ko" "fs/fuse/fuse.ko" "fs/nls/nls_utf8.ko")
+>>>>>>> b302688d4f6cc133e3b73238ff08ca4330cb64c9
 
     if [ x = "x$NO_CCACHE" ] && ccache -V &>/dev/null ; then
         CCACHE=ccache
@@ -35,6 +39,7 @@ build ()
     mkdir -p "$target_dir/usr"
     cp "$KERNEL_DIR/usr/"*.list "$target_dir/usr"
     sed "s|usr/|$KERNEL_DIR/usr/|g" -i "$target_dir/usr/"*.list
+<<<<<<< HEAD
     THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
     make -j${THREADS} ARCH=arm ${target}_cm9_defconfig
     make -j${THREADS}
@@ -42,19 +47,35 @@ build ()
     for module in "${MODULES[@]}" ; do
         cp "$target_dir/$module" $ANDROID_BUILD_TOP/device/samsung/galaxytab/modules
     done
+=======
+    mka -C "$KERNEL_DIR" O="$target_dir" ${target}\_cm9_defconfig HOSTCC="$CCACHE gcc"
+    mka -C "$KERNEL_DIR" O="$target_dir" HOSTCC="$CCACHE gcc" CROSS_COMPILE="$CCACHE $CROSS_PREFIX" zImage modules
+    cp "$target_dir"/arch/arm/boot/zImage $ANDROID_BUILD_TOP/device/samsung/galaxytab/kernel-$target
+>>>>>>> b302688d4f6cc133e3b73238ff08ca4330cb64c9
 }
     
 setup
 
+<<<<<<< HEAD
+=======
+setup
+
+>>>>>>> b302688d4f6cc133e3b73238ff08ca4330cb64c9
 if [ "$1" = clean ] ; then
     rm -fr "$BUILD_DIR"/*
     exit 0
 fi
 
+<<<<<<< HEAD
 P1_target=$1
 targets=("$@")
 if [ 0 = "${#targets[@]}" ] ; then
     targets=($P1_target)
+=======
+targets=("$@")
+if [ 0 = "${#targets[@]}" ] ; then
+    targets=(p1 p1c p1l p1n)
+>>>>>>> b302688d4f6cc133e3b73238ff08ca4330cb64c9
 fi
 
 START=$(date +%s)
